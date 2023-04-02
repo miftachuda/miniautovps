b8a125d1af2d2b06edb0384033debadb00507673
Miftachul Huda <miftachulcc@gmail.com>
2023-04-02 13:06:26 +0700

    fix systemctl wstls

--- websocket/ins-ws.sh
+++ websocket/ins-ws.sh
@@ -5,7 +5,7 @@ ghrepo="raw.githubusercontent.com/miftachuda/miniautovps/master/"
 # Setup WS nontls Service
 wget -O /usr/local/bin/ws-nontls "https://${ghrepo}/websocket/ws-nontls"
 chmod +x /usr/local/bin/ws-nontls
-mv websocket/ws-nontls.service /etc/systemd/system/ws-nontls.service
+wget -O  /etc/systemd/system/ws-nontls.service "https://${ghrepo}/websocket/ws-nontls.service"
 systemctl daemon-reload
 systemctl enable ws-nontls
 systemctl restart ws-nontls
@@ -14,7 +14,7 @@ systemctl restart ws-nontls
 # Setup WS tls Service
 wget -O /usr/local/bin/ws-tls "https://${ghrepo}/websocket/ws-tls"
 chmod +x /usr/local/bin/ws-tls
-mv websocket/ws-tls.service /etc/systemd/system/ws-tls.service
+wget -O  /etc/systemd/system/ws-tls.service "https://${ghrepo}/websocket/ws-tls.service"
 systemctl daemon-reload
 systemctl enable ws-tls
 systemctl restart ws-tls
